bd0470985411f23ed302c0a256e5fdc7b2e92691
Lucas Previato <lucaspreviato@Live.com>
2025-12-31 14:41:25 -0300

    feat: remove prioridade de variável de ambiente e adiciona configuração Amplify com pnpm

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -1,23 +1,21 @@
 // Configuração de URLs da API
-const hostname = window?.location.hostname ?? '';
+const hostname = window?.location.hostname ?? "";
 
-const isLocal = import.meta.env.VITE_ENV === 'local';
-const isStaging = hostname.includes('staging');
+const isLocal = import.meta.env.VITE_ENV === "local";
+const isStaging = hostname.includes("staging");
 
 export const urls = {
   // URL da API de Tenants
-  tenants: import.meta.env.VITE_TETRA_TENANTS_URL || 
-    (isLocal 
-      ? 'http://localhost:3334' 
-      : isStaging
-        ? 'https://staging-api.tetraeducacao.com/tenants'
-        : 'https://api.tetraeducacao.com/tenants'),
+  tenants: isLocal
+    ? "http://localhost:3334"
+    : isStaging
+      ? "https://staging-api.tetraeducacao.com/tenants"
+      : "https://tenants.tetraeducacao.com.br",
 
   // URL da API de IAM
-  iam: import.meta.env.VITE_TETRA_IAM_URL || 
-    (isLocal 
-      ? 'http://localhost:3335'
-      : isStaging
-        ? 'https://staging-api.tetraeducacao.com/iam'
-        : 'https://api.tetraeducacao.com/iam'),
+  iam: isLocal
+    ? "http://localhost:3335"
+    : isStaging
+      ? "https://staging-api.tetraeducacao.com/iam"
+      : "https://api.tetraeducacao.com/iam",
 };
